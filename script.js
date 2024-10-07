bdf0065a34316165ac564fcc3edaeb443e8db598
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-07 18:27:24 -0500

    all the loops

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-for (let i = 20; i >= 10; i--) {
-    console.log(i);
+// classic for loops
+for (let incrementor = 20; incrementor >= 10; incrementor--) {
+    console.log(incrementor);
 }
 
 for (let j = 10; j <= 20; j++) {
     console.log(j);
+}
+
+// while loop
+let count = 0;
+while (count < 10) {
+    count++;
+    console.log("while loop", count)
+}
+
+// do while loop
+let num = 0;
+do {
+    // this work
+    console.log("do while loop", num);
+    num++;
+} while (num <= 5);
+
+// for of loop
+let arr = [true, 2, "three", 4, [1, 2, 3]];
+console.log(arr);
+for (let x of arr) {
+    console.log("for of loop", x);
+}
+
+// for in loop
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+console.log(car);
+for (let key in car) {
+    console.log("for in loop", key + ": " + car[key]);
 }
